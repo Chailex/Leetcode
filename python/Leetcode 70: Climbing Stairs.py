c8d93b3f751f598ebbc0ae707f252c26765b218d
Chailex <32385484+Chailex@users.noreply.github.com>
2024-09-15 21:03:06 +0530

    Update Leetcode 70: Climbing Stairs.py

--- python/Leetcode 70: Climbing Stairs.py
+++ python/Leetcode 70: Climbing Stairs.py
@@ -1,3 +1,18 @@
+# Solution using dictionary
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = dict()
+        dp[1] = 1
+        dp[2] = 2
+
+        if n <3:
+            return n
+
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n]
+        
 # Solution using constant space
 class Solution:
     def climbStairs(self, n: int) -> int:
